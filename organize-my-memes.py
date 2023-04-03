e0a84a7c83a219f30134b1296b2b1b1f6493e830
Tonny <papazogloy59@gmail.com>
2023-04-03 15:54:30 +0300

    Added folder extensability

--- organize-my-memes.py
+++ organize-my-memes.py
@@ -9,9 +9,19 @@ from sys import argv
 from toml import load as tload
 
 
-def get_use_cases() -> dict[str, Tuple[str, str]]:
+# Type Helpers
+Config = dict[str, Tuple[str, str]]
+
+# Helper functions
+
+
+def get_config():
     with open("config.toml", "r") as file:
         return tload(file)
+
+
+def get_use_cases() -> Config:
+    return get_config()
 
 
 def clean_empty_dirs(directory: str):
@@ -22,49 +32,22 @@ def clean_empty_dirs(directory: str):
             rmdir(dir_path)
 
 
-def create_tree_structure(root: str) -> None:
-    for year in range(2017, datetime.now().year + 1):
+def create_tree_structure(root: str, config: Config) -> None:
+    starting_age: int = config["starting_age"]
+    use_cases = config["use_cases"]
+    print(use_cases)
+
+    categories: set[str] = set(
+        [case["type"] for cases in use_cases.values() for case in cases]
+    )
+
+    for year in range(starting_age, datetime.now().year + 1):
         for month in range(1, 13):
             _, days_in_month = monthrange(year, month)
             for day in range(1, days_in_month + 1):
-                makedirs(
-                    f"{root}/{year}/{month}/{day}/images",
-                    exist_ok=True,
-                )
-                makedirs(
-                    f"{root}/{year}/{month}/{day}/videos",
-                    exist_ok=True,
-                )
-                makedirs(
-                    f"{root}/{year}/{month}/{day}/documents",
-                    exist_ok=True,
-                )
-                makedirs(
-                    f"{root}/{year}/{month}/{day}/other",
-                    exist_ok=True,
-                )
-                makedirs(
-                    f"{root}/{year}/{month}/{day}/archives",
-                    exist_ok=True,
-                )
-                makedirs(
-                    f"{root}/{year}/{month}/{day}/web",
-                    exist_ok=True,
-                )
-                makedirs(
-                    f"{root}/{year}/{month}/{day}/data",
-                    exist_ok=True,
-                )
-
-                makedirs(
-                    f"{root}/{year}/{month}/{day}/music",
-                    exist_ok=True,
-                )
-
-                makedirs(
-                    f"{root}/{year}/{month}/{day}/text",
-                    exist_ok=True,
-                )
+                for category in categories:
+                    directory_path = f"{root}/{year}/{month}/{day}/{category}"
+                    makedirs(directory_path, exist_ok=True)
 
 
 def move_file_to_tree(file_path: str, root: str) -> None:
@@ -92,25 +75,27 @@ def move_file_to_tree(file_path: str, root: str) -> None:
 
 def main(directory: str) -> None:
     root_directory = directory
+    config: Config = get_config()
     tree_sert_file = path.join(root_directory, "tree-cert.txt")
 
-
-    print(get_use_cases())
-    return
-
     if not path.exists(tree_sert_file):
-        create_tree_structure(root_directory)
+        create_tree_structure(root_directory, config)
         with open(tree_sert_file, "w") as f:
             f.write("File tree successfully generated.")
     else:
         print("Tree is already created, moving on to organizing")
 
+    return
     for root, _, files in walk(directory):
         if any(d.isdigit() and len(d) == 4 for d in root.split(path.sep)):
             continue  # Skip directories that are named as a year
         for file_name in files:
             file_path = path.join(root, file_name)
-            if path.isfile(file_path) and file_name != path.basename(__file__):
+            if (
+                path.isfile(file_path)
+                and file_name != "config.toml"
+                and file_name != path.basename(__file__)
+            ):
                 print(f"Moving {file_name} to {root_directory}")
                 move_file_to_tree(file_path, root_directory)
 
